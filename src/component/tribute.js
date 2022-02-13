346bc1ed284a9f67a68a9dec5f066c43a09f72a9
Ashish Yadav <ay817985@gmail.com>
2022-02-13 19:26:35 +0530

    my second commit

---
+++ src/component/tribute.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function Main(){
+    return(
+        <>
+        <div className='Header'>
+            <h1 className='name'>Lata Mangeskar</h1><br/>
+            <h2>Best playback singer of the industry</h2>
+
+        </div>
+        <div className='img'>
+            <img src='https://akm-img-a-in.tosshub.com/aajtak/images/story/202201/lata-sixteen_nine.jpg?size=948:533'/>
+        </div>
+        <div className='para'>
+            <p>
+            Social media sites have been flooded with tributes to singer Lata Mangeshkar who breathed her last at Breach Candy hospital, Mumbai, on Sunday. From dairy brand Amul, business tycoons Anand Mahindra, Gautam Adani, singer Chinmayi Sripaada, actor Neil Nithin Mukesh to sand artist Sudarshan Pattnaik, condolences poured in over the demise of the Nightingale of India.
+
+The 92-year-old singer was laid to rest at Mumbai’s Shivaji Park with full state honours. Prime Minister Narendra Modi, Shah Rukh Khan, Ranbir Kapoor, Vidya Balan among others paid their last respects to Mangeshkar.
+            </p>
+        </div>
+        </>
+        
+    )
+}
+export default Main;
